1430143d8bfb94a064217ba184c3163a0498caa5
USER <reena110497@gmail.com>
2026-06-23 13:21:01 +0530

    23rd june 2026

---
+++ src/Practice_Java/commonElementsInArray.java
@@ -0,0 +1,19 @@
+package Practice_Java;
+
+public class commonElementsInArray {
+    public static void main(String args[])
+    {
+        int arr1[]={1,2,3,4,5};
+        int arr2[]={3,5,6,7,8};
+        for(int i=0;i<arr1.length;i++)
+        {
+            for(int j=0;j<arr2.length;j++)
+            {
+                if(arr1[i]==arr2[j])
+                {
+                    System.out.println("Common Elements =" +arr1[i]);
+                }
+            }
+        }
+    }
+}

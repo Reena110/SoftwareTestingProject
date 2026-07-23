9ff7c6dd5e05ff805a349da6b5e0518bb10bc9df
USER <reena110497@gmail.com>
2026-07-23 08:27:06 +0530

    Java Practice 23 July 2026

---
+++ src/Practice_Java/missingNumber23July2026.java
@@ -0,0 +1,15 @@
+package Practice_Java;
+
+public class missingNumber23July2026 {
+    public static void main(String args[])
+    {
+        int arr[]={1,2,3,5};
+        for(int i=0;i<arr.length-1;i++)
+        {
+            if(arr[i]+1 != arr[i+1])
+            {
+                System.out.println(arr[i]+1);
+            }
+        }
+    }
+}
